8a42a9e7102fe207d36d0fcd957d3914748f231e
Mark L <73659136+markliuyuxiang@users.noreply.github.com>
2022-11-08 12:00:23 +0800

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -8,7 +8,30 @@ class Scraper {
 
   async fetch(url) {
     this.url = url
-    this.response = await fetch(url)
+    this.response = await fetch(url,headers: {
+        'authority': 'www.google.com',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,zh-TW;q=0.5',
+        'cache-control': 'no-cache',
+         'pragma': 'no-cache',
+        'referer': 'https://www.google.com/',
+        'sec-ch-ua': '"Microsoft Edge";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
+        'sec-ch-ua-arch': '""',
+        'sec-ch-ua-bitness': '"64"',
+        'sec-ch-ua-full-version': '"107.0.1418.35"',
+        'sec-ch-ua-full-version-list': '"Microsoft Edge";v="107.0.1418.35", "Chromium";v="107.0.5304.90", "Not=A?Brand";v="24.0.0.0"',
+        'sec-ch-ua-mobile': '?1',
+        'sec-ch-ua-model': '"Nexus 5"',
+        'sec-ch-ua-platform': '"Android"',
+        'sec-ch-ua-platform-version': '"6.0"',
+        'sec-ch-ua-wow64': '?0',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36 Edg/107.0.1418.35'
+    })
 
     const server = this.response.headers.get('server')
 
